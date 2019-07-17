8f416f727eca272aa53de77e9e75b8a4258c2936
Samuel <samuelmodestoes@gmail.com>
2019-07-16 23:31:14 -0300

    Lógica da aplicação adicionada, número randomico de 0 a 1 sendo 0 = cara e 1 = coroa.

--- app/src/main/java/com/example/samue/caraoucoroa/MainActivity.java
+++ app/src/main/java/com/example/samue/caraoucoroa/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.samue.caraoucoroa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar =  findViewById(R.id.buttonJogar);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*
+                a partir do click será chamada esse start activity, que precisa de uma Intent
+                logo após instanciar a intent eu passo o contexto e a activity que eu quero abrir.
+                 */
+                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+
+                //Passar os dados da primeira activity para a activity de resultado
+                //porém antes disso é preciso gerar o rand com essas duas linhas abaixo.
+                int num = new Random().nextInt(2);
+                intent.putExtra("numero", num);
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/samue/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/com/example/samue/caraoucoroa/ResultadoActivity.java
@@ -2,12 +2,45 @@ package com.example.samue.caraoucoroa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private Button botaoVoltar;
+    private ImageView resultadoImagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        botaoVoltar = findViewById(R.id.buttonVoltar);
+        resultadoImagem = findViewById(R.id.imageResultado);
+
+        //Recuperar dados
+        Bundle dados = getIntent().getExtras();
+        int num = dados.getInt("numero");
+
+        if(num == 0){
+            resultadoImagem.setImageResource(R.drawable.moeda_cara);
+        } else {
+            resultadoImagem.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        // Evento de click para o botao voltar
+
+        botaoVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // esse método destroi a activity atual, voltando a anterior
+                // funciona com o conceito de pilhas de activitys.
+                finish();
+
+            }
+        });
+
     }
 }
